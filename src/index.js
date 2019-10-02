b6dafd847a2084d75dadacc0ab7db3365c1d7f04
Muna Jemal <muna.aman66@gmail.com>
2019-10-01 21:04:37 -0400

    Created a new react project that displayes hi

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {createStore,applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import {composeWithDevTools} from 'redux-devtools-extension'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './components/App';
+import rootReducer from './reducers/index';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import'bootstrap/dist/css/bootstrap.min.css';
+
+const composeEnhancers = composeWithDevTools({trace : true});
+const store = createStore(rootReducer);//,{},composeEnhancers(applyMiddleware()));
+
+ReactDOM.render(
+	<Provider store ={store}>
+	<App />
+	</Provider>,
+	document.getElementById('root')
+);
+
