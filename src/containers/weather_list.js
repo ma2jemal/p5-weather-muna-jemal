a9de166aab6b2754dc2f9685e8dc1544d8af0da4
Muna Jemal <muna.aman66@gmail.com>
2019-10-16 16:13:34 -0400

    4.1 Adding sparkline charts Done

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,17 +1,48 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Sparklines, SparklinesLine } from 'react-sparklines';
 
 class WeatherList extends Component{
 
 	renderWeather(cityData){
 		const name = cityData.city.name;
+		const temp = cityData.list.map(lists => (lists.main.temp));
+		const pressure = cityData.list.map(lists => (lists.main.pressure));
+		const humidity = cityData.list.map(lists => (lists.main.humidity))
 
 		return(
 			<tr key={name}>
 				<td>{ name }</td>
-				<td>{cityData.list[0].main.temp}</td>
-				<td>{ cityData.list[0].main.pressure}</td>
-				<td> { cityData.list[0].main.humidity}</td>
+				<td>
+					<div>
+						<Sparklines
+							svgHeight = {120}
+							svgWidth = {180}
+							data = {temp}
+							>
+							<SparklinesLine color = "red" />
+						</Sparklines>
+					</div>
+				</td>
+				<td><div>
+						<Sparklines
+							svgHeight = {120}
+							svgWidth = {180}
+							data = {pressure}
+							>
+							<SparklinesLine color = "blue" />
+						</Sparklines>
+					</div></td>
+				<td><div>
+						<Sparklines
+							svgHeight = {120}
+							svgWidth = {180}
+							data = {humidity}
+							>
+							<SparklinesLine color = "green" />
+						</Sparklines>
+					</div></td>
+				
 			</tr>
 			)
 	}
